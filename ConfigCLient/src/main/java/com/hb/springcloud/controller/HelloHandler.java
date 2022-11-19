490a98e3aa75fd05b84558ce508cd51271f3fa42
Lzq <Lzq@guigu.com>
2022-11-19 19:34:35 +0800

    提交2

--- ConfigCLient/src/main/java/com/hb/springcloud/controller/HelloHandler.java
+++ ConfigCLient/src/main/java/com/hb/springcloud/controller/HelloHandler.java
@@ -1,25 +1,19 @@
 package com.hb.springcloud.controller;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/native")
-public class NativeConfigHandler {
+@RequestMapping("/hello")
+public class HelloHandler {
 
-    //@Value("${server.port}")
-    private String port = "666";
-
-    //@Value("${foo}")
-    private String foo = "999";
-
-    //有问题@Value("${server.port}")  @Value("${foo}"）
+    @Value("${server.port}")
+    private String port;
 
     @GetMapping("/index")
     public String index(){
-        return this.port+"-"+this.foo;
+        return this.port;
     }
 }
